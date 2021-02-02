4ada5be61be13b03ddfb9cd591cf1e767dd7526f
Christopher Sauer <sauer@mfk.fau.de>
2021-02-02 17:45:41 +0100

    Enviroment.yml and example.pdf with export added

--- wave_voice.py
+++ wave_voice.py
@@ -8,6 +8,7 @@
 # IMPORT
 # ==============================================================================
 import os
+import subprocess
 from google.cloud import texttospeech
 
 
@@ -20,16 +21,32 @@ os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'credentials.json'
 # ==============================================================================
 # DEFINITION
 # ==============================================================================
-def synthesize_text(text: str):
+def convert_pdf_to_string(pdf_filename: str) -> str:
+    """Convert pdf_filename to string"""
+    subprocess.call(['pdftotext', '-enc', 'UTF-8', pdf_filename])
+    txt_file = open(pdf_filename[:-3] + 'txt', 'r', encoding='UTF-8')
+    return txt_file.read()
+
+
+def synthesize_text(text: str, language='DE'):
     """Synthesize text from String and write to output.mp3"""
     client = texttospeech.TextToSpeechClient()
     input_text = texttospeech.SynthesisInput(text=text)
 
-    voice = texttospeech.VoiceSelectionParams(
-        language_code="de-DE",
-        name="de-DE-Wavenet-F",
-        ssml_gender=texttospeech.SsmlVoiceGender.FEMALE
-    )
+    if language == 'DE':
+        voice = texttospeech.VoiceSelectionParams(
+            language_code="de-DE",
+            name="de-DE-Wavenet-F",
+            ssml_gender=texttospeech.SsmlVoiceGender.FEMALE
+        )
+    elif language == 'EN':
+        voice = texttospeech.VoiceSelectionParams(
+            language_code="en-US",
+            name="en-US-Wavenet-F",
+            ssml_gender=texttospeech.SsmlVoiceGender.FEMALE
+        )
+    else:
+        raise NotImplementedError
 
     audio_config = texttospeech.AudioConfig(
         audio_encoding=texttospeech.AudioEncoding.MP3
@@ -47,7 +64,8 @@ def synthesize_text(text: str):
 
 def main():
     """Main method"""
-    synthesize_text("Hallo, Welt!")
+    text = convert_pdf_to_string('example.pdf')
+    synthesize_text(text, language='EN')
 
 
 if __name__ == "__main__":
